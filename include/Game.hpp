7bd14c03cbe69b6ba459fa7e800c76f22eaf82b0
Mark Oates <markoates0@gmail.com>
2020-08-22 15:11:21 -0400

    * Add empty API methods for game

--- include/Game.hpp
+++ include/Game.hpp
@@ -3,6 +3,7 @@
 
 #include <Card.hpp>
 #include <Deck.hpp>
+#include <string>
 #include <tuple>
 #include <vector>
 
@@ -14,6 +15,7 @@ private:
    std::vector<Card> waste;
    std::vector<std::vector<std::tuple<bool, Card>>> tableau;
    std::vector<std::vector<std::tuple<bool, Card>>> foundations;
+   std::string state;
 
 public:
    Game();
@@ -25,8 +27,16 @@ public:
    std::vector<std::vector<std::tuple<bool, Card>>> get_tableau();
    std::vector<std::vector<std::tuple<bool, Card>>> get_foundations();
 void start();
+void flip_topmost_card_on_tableau_column_to_face_up(int tableau_column_num=0);
 void place_top_stock_card_on_waste();
 void place_top_waste_card_on_foundation(int foundation_index_num=0);
+void replenish_waste_with_stock();
+void move_cards_from_tableau_row_to_other_tableau_row(int source_tableau_column_num=0, int destination_tableau_column_num=0, int num_cards_from_topmost_card=0);
+void move_topmost_tableau_row_card_to_foundation(int tableau_column_num=0);
+void set_state_playing();
+void check_game_win_or_lose();
+void set_state_game_won();
+void set_state_game_lost();
 int stock_num_cards();
 };
 
--- src/Game.cpp
+++ src/Game.cpp
@@ -11,6 +11,7 @@ Game::Game()
    , waste({})
    , tableau({})
    , foundations({})
+   , state("playing")
 {
 }
 
@@ -56,6 +57,15 @@ waste.clear();
 foundations.clear();
 foundations.resize(4);
 
+set_state_playing();
+
+return;
+
+}
+
+void Game::flip_topmost_card_on_tableau_column_to_face_up(int tableau_column_num)
+{
+// TODO
 return;
 
 }
@@ -70,6 +80,55 @@ return;
 void Game::place_top_waste_card_on_foundation(int foundation_index_num)
 {
 // TODO
+return;
+
+}
+
+void Game::replenish_waste_with_stock()
+{
+// TODO
+return;
+
+}
+
+void Game::move_cards_from_tableau_row_to_other_tableau_row(int source_tableau_column_num, int destination_tableau_column_num, int num_cards_from_topmost_card)
+{
+// TODO
+return;
+
+}
+
+void Game::move_topmost_tableau_row_card_to_foundation(int tableau_column_num)
+{
+// TODO
+return;
+
+}
+
+void Game::set_state_playing()
+{
+state = "playing";
+return;
+
+}
+
+void Game::check_game_win_or_lose()
+{
+// TODO
+return;
+
+}
+
+void Game::set_state_game_won()
+{
+state = "game_won";
+return;
+
+}
+
+void Game::set_state_game_lost()
+{
+state = "game_over";
 return;
 
 }
