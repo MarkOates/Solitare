0dcf25cc2a5d842f61b140038032be182bdbac9e
Mark Oates <markoates0@gmail.com>
2020-08-22 17:46:45 -0400

    * Add more comments to methods for requirements

--- src/Game.cpp
+++ src/Game.cpp
@@ -47,6 +47,8 @@ std::vector<std::vector<Deck>> Game::get_foundations()
 
 void Game::start()
 {
+// Rules of solitare: https://bicyclecards.com/how-to-play/solitaire/
+
 stock = DeckFactory::generate_classic_52_playing_card_deck();
 
 tableau.clear();
@@ -66,15 +68,17 @@ return;
 void Game::flip_topmost_card_on_tableau_column_to_face_up(int tableau_column_num)
 {
 // tableau_column_num must be valid
-// 
+// if topmost card is already flipped up, just return
 
-// if topmost card is already flipped up, do nothing
+// TODO
 return;
 
 }
 
 void Game::place_top_stock_card_on_waste()
 {
+// if stock is empty, raise exception
+
 // TODO
 return;
 
@@ -82,6 +86,9 @@ return;
 
 void Game::place_top_waste_card_on_foundation(int foundation_index_num)
 {
+// if waste is empty, raise exception
+// if top waste card does not fit onto any foundations, raise exception
+
 // TODO
 return;
 
@@ -99,6 +106,11 @@ return;
 
 void Game::move_cards_from_tableau_row_to_other_tableau_row(int source_tableau_column_num, int destination_tableau_column_num, int num_cards_from_topmost_card)
 {
+// source_tableau_column_num must be valid
+// destination_tableau_column_num must be valid
+// num_cards_from_topmost_card must not exceed size of cards in tableau row
+// num_cards_from_topmost_card must be face up
+
 // TODO
 return;
 
